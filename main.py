0ea5889c7f40339a909c4b5bdb6a70f637fcdaf3
Szymon Baczynski <simon.baczynski@gmail.com>
2020-10-08 01:04:38 +0200

    Basic notation and To Do List

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is project:
+# Project_COVID-19_World_and_Polish_MZ_Twitter
+# Author: Szymon Baczyński
+# Start date: 10 / 2020
+# Target: Build Python script where data about COVID-19 will be processed and visualize
+# To Do List:
+#       1) Visualization for Global data:
+#           1.1) Download data from GitHub: https://github.com/CSSEGISandData/COVID-19
+#               1.1.1) Vis for: time_series_covid19_confirmed_global.csv = https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
+#       2) Visualization for data from Poland - Twitter = Ministry of Health (Poland)
+#           2.1) Scraping Twitter: https://twitter.com/MZ_GOV_PL
+#               2.1.1) Vis deaths from Twitter (scrap + regex)
